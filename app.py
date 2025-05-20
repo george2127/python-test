8e472dc34e717c8b6aa9cb9e3f90da9c00ab0827
george2127 <georgedotreece@gmail.com>
2025-05-21 04:35:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
     return 'Hello World! This is my Fucking CodePipeline Project and Successfully Running and Fucking Running........!!! I\'m host %s' % socket.gethostname()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
