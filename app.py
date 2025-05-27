fd676205c598fc49561812178fe84db55dc27cba
george2127 <georgedotreece@gmail.com>
2025-05-27 09:18:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import socket
+from mangum import Mangum
 
 app = Flask(__name__)
 
@@ -7,5 +8,4 @@ app = Flask(__name__)
 def hello():
     return 'Hello World! loola pai gia I\'m host %s' % socket.gethostname()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+handler = Mangum(app)  # Lambda entry point
